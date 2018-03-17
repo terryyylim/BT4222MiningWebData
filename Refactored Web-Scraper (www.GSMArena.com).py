400c82896b6c7a23210c6f4fb09fac56a9a498d5
PC0EBJHJ\Terence Lim <terencelimxp@gmail.com>
2018-03-17 14:10:44 +0800

    added: function to scrape time of review

--- Refactored Web-Scraper (www.GSMArena.com).py
+++ Refactored Web-Scraper (www.GSMArena.com).py
@@ -6,12 +6,16 @@ class Phone:
         self.main_url = main_url
         self.comment_urls = []
         self.comments = []
+        self.comments_date = []
         
     def store_comment_urls(self, url):
         self.comment_urls.append(url)
         
     def store_comment(self, comment):
         self.comments.append(comment)
+
+    def store_comment_date(self, date):
+        self.comments_date.append(date)
 
 # Importing required libraries for scraping
 import ssl
@@ -22,8 +26,12 @@ from multiprocessing import Process
 from bs4 import BeautifulSoup as BS
 
 # Importing required libraries for wordprocessing
+from datetime import datetime as dt
+import time
+from time import mktime
 import pandas as pd
 import numpy as np
+import json
 
 # Helper functions
 # is_number method is to filter out ">>" text that are present in models with more than 3 pages of comments
@@ -45,14 +53,14 @@ def is_number(s):
 
 def get_phone_brand_links(website):
     r = requests.get(website)
-    data = BS(r.content, "lxml")
+    data = BS(r.content, "html.parser")
     all_brands_link = [website + extension for extension in (brand['href'] for brand in ((data.find("div", {"class": "brandmenu-v2"})).find("ul")).find_all("a"))]
     return all_brands_link
     
 def get_model_name_link(website):
     try:
         r = requests.get(website, timeout=10)
-        data = BS(r.content, "lxml")
+        data = BS(r.content, "html.parser")
         # getting name of brand to append to correct key in 'all_brands_phones' dictionary
         name = re.split(" ", ((data.find("div", {"class": "article-hgroup"})).find("h1", {"class": "article-info-name"})).text)[0]
         # storing only titles of links related to phones (to filter tablets,watches etc.) using boolean checker
@@ -84,7 +92,7 @@ def get_model_comments_urls(phone):
     try:
         url = phone.main_url
         r = requests.get(url, timeout=10)
-        model_mainpage = BS(r.content, "lxml")
+        model_mainpage = BS(r.content, "html.parser")
         #print(model_mainpage)
         if ((model_mainpage.find("div", {"id": "user-comments"})) != None):
             
@@ -100,14 +108,17 @@ def get_model_comments_urls(phone):
                 comments_mainpage = [(home_url + to_comment["href"]) for to_comment in (model_mainpage.find("div", {"class": "button-links"})).find_all("a")][0]
 
                 r = requests.get(comments_mainpage, timeout=10)
-                comments_homepage = BS(r.content, "lxml")
+                comments_homepage = BS(r.content, "html.parser")
                 
                 wanted = [int(number.text) for number in ((comments_homepage.find("div", {"class": "nav-pages"})).find_all("a")) if (is_number(number.text) == True)]
 
+                # store main page of comments' URL first
+                main_url = home_url + main_comments_url + ".php"
+                phone.store_comment_urls(main_url)
                 # case: more than 1 page worth of comments available           
                 if ((len(wanted)) != 0):
                     number_of_pages = max(wanted)
-
+            
                     for pg_number in range(2, number_of_pages+1):
                         indv_page_url = home_url + main_comments_url + "p" + str(pg_number) + ".php"
                         phone.store_comment_urls(indv_page_url)
@@ -121,7 +132,7 @@ def get_model_comments(phone):
     try:
         for review_pagelink in phone.comment_urls:
             r = requests.get(review_pagelink, timeout=10)
-            review_page = BS(r.content, "lxml")
+            review_page = BS(r.content, "html.parser")
 
             all_comments = review_page.find_all("p", {"class": "uopin"})
             for comment in all_comments:
@@ -149,6 +160,25 @@ def get_model_comments(phone):
         print("Oops SSLError faced on " + phone)
     except requests.exceptions.Timeout:
         print("Timeout occured in get_model_comments_urls function")
+
+def get_model_comments_date(phone):
+    try:
+        for review_pagelink in phone.comment_urls:
+            r = requests.get(review_pagelink, timeout=10)
+            review_page = BS(r.content, "html.parser")
+
+            all_dates = review_page.find_all("li", {"class": "upost"})
+            for date in all_dates:
+                date = date.get_text()
+                if (('hour' in date) or ('hours' in date)) :
+                    date = dt.now().date()
+                else:
+                    date = dt.strptime(date, '%d %b %Y').date()
+                phone.store_comment_date(date)
+    except ssl.SSLError:
+        print("Oops SSLError faced on " + phone)
+    except requests.exceptions.Timeout:
+        print("Timeout occured in get_model_comments_date function")
     
 # Running the program
 if __name__ == '__main__':
@@ -167,36 +197,48 @@ if __name__ == '__main__':
         # Getting a list of all phone objects
         for brand in phones:
             for model in brand:
-                all_phones.append(model)
+                if (model is not None):
+                    all_phones.append(model)
+                else:
+                    print("skipping " + model.name)
         print(all_phones)
         print("Finished storing all phone objects into a list")
         
         # Storing list of all models review url into phone object
         #for brand in range(len(all_phones)):
-        for brand in range(3):
+        for brand in range(1): # use above for loop to scrape information for ALL phones
             get_model_comments_urls(all_phones[brand])
             print("Done with " + all_phones[brand].model + " review URLs!")
             print(all_phones[brand].comment_urls)
 
         # Storing list of all models review into phone object    
         #for brand in range(len(all_phones)):
-        for brand in range(3):
+        for brand in range(1): # use above for loop to scrape information for ALL phones
             get_model_comments(all_phones[brand])
             print("Done with " + all_phones[brand].model + " reviews!")
             print(all_phones[brand].comments)
 
+        # Storing list of all models review dates into phone object
+        #for brand in range(len(all_phones)):
+        for brand in range(1): # use above for loop to scrape information for ALL phones
+            get_model_comments_date(all_phones[brand])
+            print("Done with " + all_phones[brand].model + " date of reviews!")
+            print(all_phones[brand].comments_date)
+
         # Creating DataFrame to store phone model comments
-        phone_df = pd.DataFrame(columns=['phone_brand', 'phone_model', 'phone_reviews'])
+        phone_df = pd.DataFrame(columns=['phone_brand', 'phone_model', 'phone_reviews', 'phone_reviews_dates'])
 
         print("Adding into dataframe now...")
         # Append all phone information to DataFrame
         #for phone in range(len(all_phones)):
-        for phone in range(3):
+        for phone in range(1):
             phone_brand = all_phones[phone].brand
             phone_model = all_phones[phone].model
             phone_reviews = all_phones[phone].comments
-            new_row = [phone_brand, phone_model, phone_reviews]
+            phone_reviews_dates = all_phones[phone].comments_date
+            new_row = [phone_brand, phone_model, phone_reviews, phone_reviews_dates]
             phone_df.loc[len(phone_df)] = new_row
 
         phone_df.to_csv("test.csv", encoding='utf-8', index=False)
+        phone_df.to_json(orient='split')
         print(phone_df)
